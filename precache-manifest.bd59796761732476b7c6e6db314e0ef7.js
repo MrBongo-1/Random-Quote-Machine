ee76fb60a2c6b6a854250045f3b46c90d781c82f
Mr. B <brofan66@gmail.com>
2020-12-28 15:59:40 -0500

    Updates

---
+++ precache-manifest.bd59796761732476b7c6e6db314e0ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4504b6a3c293903ad0bd935b415e5e2",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "6a636015efd53f249a98",
+    "url": "/Random-Quote-Machine/static/css/main.cc80568f.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/Random-Quote-Machine/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a636015efd53f249a98",
+    "url": "/Random-Quote-Machine/static/js/main.850fd948.chunk.js"
+  },
+  {
+    "revision": "857d28a68373431d086b",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.ae688bef.js"
+  }
+]);
